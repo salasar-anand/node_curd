a2b8360ae6e5e9be6cb0f33f172083cca874ec0e
salasar-anand <145999616+salasar-anand@users.noreply.github.com>
2024-03-29 12:34:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,19 +19,19 @@ mongoose.connect(MONGO_DB_CONFIG.DB, {
     }
 );
 
-app.use(cors({ 'origin': '*' }));
+// app.use(cors({ 'origin': '*' }));
 app.get('/', function (req, res) {
     res.send('This is the Home Page');
 });
  
 //
 app.use(express.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 app.use('/uploads', express.static('uploads'));
 app.use('/api', require('./routes/app.route'));
 app.use(error.errorHandler);
