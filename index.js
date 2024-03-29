920cb29e31b18f82cfa44ea19e81a10b29958ffe
salasar-anand <145999616+salasar-anand@users.noreply.github.com>
2024-03-29 12:26:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ app.get('/', function (req, res) {
  
 //
 app.use(express.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use('/uploads', express.static('uploads'));
 app.use('/api', require('./routes/app.route'));
 app.use(error.errorHandler);
